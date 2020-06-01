998bad563c41269d9b161566ac1fcfce111b2dd6
Pedro'Korwa <59316805+Pedro-korwa@users.noreply.github.com>
2020-06-01 21:17:51 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-//function expersion
+//function EXPRESSION
 
 var random = function (){
 	var komputer = Math.floor(Math.random()*5);
@@ -21,4 +21,27 @@ var random = function (){
 
 	return komputer;
 }
+
+// function DECLARATION
 alert (random());
+function random (){
+	var komputer = Math.floor(Math.random()*5);
+
+		  if (komputer < 1){
+		   	  komputer = 'gula';
+	 }
+	 else if (komputer >1 && komputer <= 2){
+	 	komputer = 'teh';
+	 }
+	 else if (komputer >2 && komputer <=3){
+	 	komputer= 'kopi'
+	 }
+	 else if (komputer >3 && komputer <=4){
+	 	komputer= 'tepung'
+	 }
+	 else {
+	 	komputer = 'minyak';
+	 }
+
+	return komputer;
+}
