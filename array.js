13e54ef8076f2d8edda18909bbbd4c362fcc255b
Pedro'Korwa <59316805+Pedro-korwa@users.noreply.github.com>
2020-06-04 15:56:45 +0700

    15:56

--- array.js
+++ array.js
@@ -40,3 +40,50 @@ var napi = ['pedro', 'berthus', 'bertho', 2, 4, true, false , 'kucing', ' kam', 
    }
 
    console.log(user());
+
+
+
+
+
+
+
+                             //PENAMBAHAN BEBERAPA METHOD PADA ARRAY
+//manipulasi array
+
+var napi = ['zoro', 'war', 'bobi', 2, 4, true, false , 'kucing', ' kam', 'semua'];
+   for (i =0; i < napi.length; i++){
+  	// console.log(napi[i]);
+  }  
+ var pedro = ['bobi','war','zoro'];
+   for (i = 0; i < pedro.length; i++){
+   	// console.log(pedro.join(' '));
+   }
+//membuat array dengan mengunakan function
+  function sa (){
+  	var nama = ['bobi','war','zoro'];
+  	 for (i = 0; i < sa.length; i++){
+
+  	 }
+  	 return nama;
+  }
+   // method pada array 
+// Membuat function yang memiki array yang Membangkitkan bilangan random dari 1 sampai 5 
+ var acak = ['siapa', 'saja', 'yang', 'datang', 'ke', 'sa', 'punya', 'rumah'];
+// 1 pengunaan join untuk mengabungkan beberapa element menjadi string
+   for (b = 0; b < acak.length; b++){
+   	// console.log(acak.join(' '));
+   }
+// 2 pengunaan push dan pop [bekerja pada element terakhir pada array]
+var arr = ['bobi', 'war', 'zoro'];
+ for (i = 0; i <= arr.length; i++){
+
+ }
+arr.push('Mantap', 'sekali', 'Ganteng');
+// 3 pengunaan pop menghapus element terakhir pada array
+arr.pop();
+// 4 pengunaan shift dan unshift [bekerja pada element terakhir pada array]
+//menambah di awal element
+arr.unshift('pace');
+//shift menghilangkan element pertama pada array
+arr.shift();
+console.log(arr.join(' '));
