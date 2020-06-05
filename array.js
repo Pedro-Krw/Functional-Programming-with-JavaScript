28dedad9432105afc48e9429d129ab0feef9158f
Pedro'Korwa <59316805+Pedro-korwa@users.noreply.github.com>
2020-06-05 11:39:07 +0700

    11:39

--- array.js
+++ array.js
@@ -1,62 +1,12 @@
 //manipulasi array
 
-var napi = ['pedro', 'berthus', 'bertho', 2, 4, true, false , 'kucing', ' kam', 'semua'];
+var napi = ['zoro', 'war', 'bobi', 2, 4, true, false , 'forEach', 'Boolean', 'Function'];
    for (i =0; i < napi.length; i++){
-  	// console.log(napi[i]);
+  	console.log(napi[i]);
   }  
-
- var pedro = ['Pedro','Teriyanus','Korwa'];
-   for (i = 0; i < pedro.length; i++){
-   	// console.log(pedro.join(' '));
-   }
-//membuat array dengan mengunakan function
-  function sa (){
-  	var nama = ['Pedro','Teriyanus','Korwa'];
-  	 for (i = 0; i < sa.length; i++){
-
-  	 }
-  	 return nama;
-  }
-// console.log(sa().join(' '));
-// Membuat function yang memiki array yang Membangkitkan bilangan random dari 1 sampai 5 
-   function user (){
-   	 var  komputer =  Math.floor(Math.random()*5);
-   	 if (komputer <1 ){
-   	 	komputer = 'Satu';
-   	 }
-   	 else if (komputer > 1 && komputer <= 2){
-   	 	komputer = 'dua';
-   	 }
-   	 else if (komputer > 2 && komputer <= 3){
-   	 	komputer = 'Tiga';
-   	 }
-   	 else if (komputer > 3 && komputer <= 4){
-   	 	komputer = 'Empat';
-   	 }
-   	 else {
-   	 	komputer = 'Lima';
-   	 }
-  return komputer;
-   }
-
-   console.log(user());
-
-
-
-
-
-
-
-                             //PENAMBAHAN BEBERAPA METHOD PADA ARRAY
-//manipulasi array
-
-var napi = ['zoro', 'war', 'bobi', 2, 4, true, false , 'kucing', ' kam', 'semua'];
-   for (i =0; i < napi.length; i++){
-  	// console.log(napi[i]);
-  }  
- var pedro = ['bobi','war','zoro'];
-   for (i = 0; i < pedro.length; i++){
-   	// console.log(pedro.join(' '));
+ var dia = ['bobi','war','zoro'];
+   for (i = 0; i < dia.length; i++){
+   	console.log(dia.join(' '));
    }
 //membuat array dengan mengunakan function
   function sa (){
@@ -67,19 +17,18 @@ var napi = ['zoro', 'war', 'bobi', 2, 4, true, false , 'kucing', ' kam', 'semua'
   	 return nama;
   }
    // method pada array 
-// Membuat function yang memiki array yang Membangkitkan bilangan random dari 1 sampai 5 
- var acak = ['siapa', 'saja', 'yang', 'datang', 'ke', 'sa', 'punya', 'rumah'];
+ var acak = ['siapa', 10,'saja', 12,'yang', 22,'datang', 4,'ke',0, 'sa', 'punya', 'rumah'];
 // 1 pengunaan join untuk mengabungkan beberapa element menjadi string
    for (b = 0; b < acak.length; b++){
-   	// console.log(acak.join(' '));
+   	console.log(acak.join(' '));
    }
 // 2 pengunaan push dan pop [bekerja pada element terakhir pada array]
 var arr = ['bobi', 'war', 'zoro', ' true', 'false'];
  for (i = 0; i <= arr.length; i++){
 
  }
-// arr.push('Boolean', 'False', 'True');
-// 3 pengunaan pop menghapus element terakhir pada array
+arr.push('Boolean', 'False', 'True');
+//3 pengunaan pop menghapus element terakhir pada array
 arr.pop();
 // 4 pengunaan shift dan unshift [bekerja pada element terakhir pada array]
 //menambah di awal element
@@ -90,6 +39,22 @@ arr.shift();
 arr.splice(2, 0, 'Waramber');
 //membuat array baru dan mengambil element array ke dalamnya [slice awal dan akhir]
 arr2 = arr.slice(1,4)
+// mathod forEach dan map pada array [looping]
+var angka = [1,2,3,4,5,6,7,8,9,'sapi'];
+angka.forEach( function(r){
+    console.log(r);
+});
 console.log(arr.join(' '));
-// console.log(arr2.join(' '));
+console.log(arr2.join(' '));
 
+var insos = ['pinang', 'sirih', 'kapur', true, false];
+
+insos.forEach(function(e) {
+    console.log(e);
+});
+
+var bilangan = [2,4,5,7,8,3,1,0,10,9];
+bilangan.sort(function(a,b){
+	 return a-b;
+})
+console.log(bilangan.join(' '))
